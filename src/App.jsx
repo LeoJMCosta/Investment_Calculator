58f2c8538bb79c35bffeb6bd398636149b6742f7
Leonardo Costa <l.j.m.costa@hotmail.com>
2024-05-15 21:18:22 +0100

    App had some flaws, conditionals added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
     duration: 10
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -22,7 +25,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChangeInput={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className='center'>Please enter a duration greater than 0</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
